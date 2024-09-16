313c2024c06f84ed5fe589e9190ce15b1dbd6fff
Besal Raj <besalraj@Besals-MacBook-Pro.local>
2024-09-16 14:37:40 +0530

    Feat : Add keyboard navigation

--- src/Components/SuggestionsList.jsx
+++ src/Components/SuggestionsList.jsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react";
+
 const SuggestionsList = ({suggestions=[], highlight, dataKey, onSuggestionClick}) => {
+
+    const [activeIndex, setActiveIndex] = useState(-1);
 
     const getHighlightedText = (text, highlight) => {
         const parts = text.split(new RegExp(`(${highlight})`, "gi"));
@@ -11,6 +15,29 @@ const SuggestionsList = ({suggestions=[], highlight, dataKey, onSuggestionClick}
         </span>
     }
 
+    const handleKeyDown = (e) => {
+        if(e.key === 'ArrowDown') {
+            setActiveIndex((prevIndex) => prevIndex < suggestions.length -1 ? prevIndex +1 : 0);
+        } else if(e.key === 'ArrowUp')
+        {
+            setActiveIndex((prevIndex) => prevIndex > 0 ? prevIndex-1 : suggestions.length -1);
+        } else if(e.key === 'Enter')
+        {
+            if(activeIndex>=0)
+            {
+                onSuggestionClick(suggestions[activeIndex]);
+            }
+        }
+    }
+
+    useEffect(() => {   
+        document.addEventListener('keydown', handleKeyDown);
+
+        return () => document.removeEventListener('keydown', handleKeyDown);
+    }, [activeIndex, suggestions]);
+
+
+
     return (
         <>
             {suggestions.map((suggestion, index) => {
@@ -19,7 +46,10 @@ const SuggestionsList = ({suggestions=[], highlight, dataKey, onSuggestionClick}
 
                 return (
                     <li key={index} onClick={() => onSuggestionClick(suggestion)}
-                    className="suggestion-item" >
+                    className={`suggestion-item ${index===activeIndex ? 'active' : ''}`} style={{
+                        backgroundColor: index === activeIndex ? '#e0e0e0' : 'transparent'
+                    }}
+>
                         {highlightedText}
                     </li>
                 )
